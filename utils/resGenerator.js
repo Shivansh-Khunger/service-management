e4dc4488699ae8b69e28b0ad71e979342f7f62d6
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-02-21 21:46:29 +0530

    add utlis functions

---
+++ utils/resGenerator.js
@@ -0,0 +1,12 @@
+function generateRes() {
+  const resObject = {
+    isSuccess: false,
+    hasError: false,
+    message: "",
+    data: null,
+  };
+
+  return resObject;
+}
+
+export default generateRes;
