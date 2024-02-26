03578d3068534ad8f2a0ed7ad169bcbd1bc004e5
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-02-26 23:48:56 +0530

    add newProduct controller

---
+++ controllers/product/productPrimitveOps.js
@@ -0,0 +1,52 @@
+import product from "../../models/product.js";
+
+import ResponsePayload from "../../utils/resGenerator.js";
+
+export async function newProduct(req, res, next) {
+	const resPayload = new ResponsePayload();
+
+	try {
+		const newProduct = await product.create({
+			// Product identification
+			name: req.body.product.name,
+			brandName: req.body.product.brandName,
+			description: req.body.product.description,
+
+			// Stock information
+			openingStock: req.body.product.openingStock,
+			stockType: req.body.product.stockType,
+
+			// Pricing
+			unitMrp: req.body.product.unitMrp,
+			sellingPrice: req.body.product.sellingPrice,
+
+			// Product details
+			batchNo: req.body.product.batchNo,
+			manufacturingDate: req.body.product.manufacturingDate,
+			expiryDate: req.body.product.expiryDate,
+			attributes: req.body.product.attributes,
+
+			// Images
+			images: req.body.product.images,
+
+			// Business and user information
+			businessId: req.body.product.businessId,
+			userId: req.body.product.userId,
+
+			// Country code
+			countryCode: req.body.product.countryCode,
+		});
+
+		resPayload.setSuccess(
+			`-> business with name -:${req.body.product.name} created by user with id -: ${eq.body.product.userId} under the business -: ${req.body.product.businessId}`,
+			newProduct,
+		);
+
+		res.log.info(resPayload, "-> response for newProduct function");
+		return res.status(201).json(resPayload);
+	} catch (err) {
+		err.funcName = `newProduct`;
+
+		next(err);
+	}
+}
