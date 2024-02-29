207408ff9f22f01faa2f6857b07d5ac1806f0938
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-03-01 02:57:50 +0530

    update checkForDevice with comments

--- middlewares/deviceCheck.js
+++ middlewares/deviceCheck.js
@@ -1,15 +1,22 @@
+// Import the UAParser library for parsing User-Agent strings
 import UAParser from "ua-parser-js";
 
+// Middleware function to check the device type
 function checkForDevice(req, res, next) {
-	const uap = new UAParser(req.headers["user-agent"]);
+    // Create a new UAParser instance and parse the User-Agent string from the request headers
+    const uap = new UAParser(req.headers["user-agent"]);
 
-	const reqDevice = uap.getDevice();
+    // Get the device information from the parsed User-Agent string
+    const reqDevice = uap.getDevice();
 
-	if (reqDevice.type !== "mobile" && reqDevice.type !== "tablet") {
-		req.checkImei = false;
-	}
+    // If the device type is not "mobile" or "tablet", set the "checkImei" property of the request to false
+    if (reqDevice.type !== "mobile" && reqDevice.type !== "tablet") {
+        req.flags.checkImei = false;
+    }
 
-	next();
+    // Proceed to the next middleware function
+    next();
 }
 
+// Export the middleware function
 export default checkForDevice;
