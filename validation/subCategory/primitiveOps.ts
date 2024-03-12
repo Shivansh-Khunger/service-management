e0b38eea60c53e92412affb6ea538be29945aaac
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-03-12 14:47:58 +0530

    add input-validation for sub-category

--- validation/subCategory/primitiveOps.ts
+++ validation/subCategory/primitiveOps.ts
@@ -1,11 +1,11 @@
 import Joi from "joi";
 
-export const newSubCategorySchema = Joi.object({
+export const newSubCategory = Joi.object({
 	name: Joi.string().required(), // The name of the category is required and must be a string
 	image: Joi.string(), // The image associated with the category must be a string
 	description: Joi.string(), // The description of the category must be a string
 });
 
-export const delSubCategorySchema = Joi.object({
-	categoryId: Joi.string().length(24).required(),
+export const delSubCategory = Joi.object({
+	subCategoryName: Joi.string().required(),
 });
