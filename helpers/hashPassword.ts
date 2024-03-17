c5999a77b9abfe662c2c28a3c8a9b0d58cc1f05e
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-03-17 20:37:13 +0530

    decrease salt rounds (now onpar with industry)

--- helpers/hashPassword.ts
+++ helpers/hashPassword.ts
@@ -5,8 +5,8 @@ async function hashPassword(nonHashedPassword: string) {
 	// Initialize an empty string for the hashed password
 	let hashedPassword = "";
 
-	// Use bcrypt to hash the password with a salt round of 15
-	bcrypt.hash(nonHashedPassword, 15).then((hash) => {
+	// Use bcrypt to hash the password with a salt round of 13
+	bcrypt.hash(nonHashedPassword, 13).then((hash) => {
 		// Once the password is hashed, assign the hashed password to the variable
 		hashedPassword = hash;
 	});
