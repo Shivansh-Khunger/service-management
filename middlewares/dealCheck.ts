acfcc077d3a568ce8d95286feaed50a63a9f855b
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-03-17 20:14:17 +0530

    add helperr and middleware to check for deal

---
+++ middlewares/dealCheck.ts
@@ -0,0 +1,103 @@
+// Import types
+import type { NextFunction, Request, Response } from "express";
+import type mongoose from "mongoose";
+import type { T_idDeal } from "../models/deals";
+
+// Import necessary modules
+import ifDealExists from "../helpers/models/dealExists";
+import validateDocumentExistence from "./helpers/valDocExistence";
+
+import CustomError from "../utils/customError";
+import augmentAndForwardError from "../utils/errorAugmenter";
+
+type DealCheckOptions =
+	| {
+			checkIn: "body";
+			bodyEntity: string;
+			entity: string;
+			passIfExists: boolean;
+			key: keyof T_idDeal;
+	  }
+	| {
+			checkIn: "query" | "params";
+			bodyEntity: undefined | null;
+			entity: string;
+			passIfExists: boolean;
+			key: keyof T_idDeal;
+	  };
+
+// Middleware function to check if a deal exists
+
+const checkForDeal = ({
+	checkIn,
+	bodyEntity,
+	entity,
+	passIfExists,
+	key = "name",
+}: DealCheckOptions) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		// Define the function name for error handling
+		const collectionName = "Deal";
+		const funcName = "checkForDeal";
+
+		let valToKey: string;
+		let dealExists: { _id: mongoose.Types.ObjectId } | null | undefined;
+
+		let errMessage: string;
+		try {
+			switch (checkIn) {
+				case "body":
+					valToKey = req.body[bodyEntity][entity];
+					dealExists = await ifDealExists(next, {
+						[key]: valToKey,
+					});
+
+					validateDocumentExistence({
+						nextFuncion: next,
+						docExists: dealExists,
+						passIfExists: passIfExists,
+						collection: collectionName,
+						collectionAttr: valToKey,
+					});
+					break;
+
+				case "query":
+					// added for future use
+					break;
+
+				case "params":
+					valToKey = req[checkIn][entity];
+					dealExists = await ifDealExists(next, {
+						[key]: valToKey,
+					});
+
+					validateDocumentExistence({
+						nextFuncion: next,
+						docExists: dealExists,
+						passIfExists: passIfExists,
+						collection: collectionName,
+						collectionAttr: valToKey,
+					});
+					break;
+
+				default: {
+					errMessage = `the request could not be completed because the checkIn-: ${checkIn} is not supported.`;
+					const err = new CustomError(errMessage);
+
+					throw err;
+				}
+			}
+		} catch (err) {
+			// Handle the caught error by passing it to the augmentAndForwardError function which will pass it to the error handling middleware
+			augmentAndForwardError({
+				next: next,
+				err: err,
+				funcName: funcName,
+				errStatus: 400,
+			});
+		}
+	};
+};
+
+// Export the middleware function
+export default checkForDeal;
