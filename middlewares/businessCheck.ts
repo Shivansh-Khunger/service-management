83b9399023a1d21d05a02d76dc6a16b89152e42b
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-03-17 20:38:14 +0530

    made every middleware max modular

--- middlewares/businessCheck.ts
+++ middlewares/businessCheck.ts
@@ -1,42 +1,103 @@
 // Import types
-import type { RequestHandler } from "express";
+import type { NextFunction, Request, Response } from "express";
+import type mongoose from "mongoose";
+import type { T_idBusiness } from "../models/business";
 
 // Import necessary modules
-import business from "../models/business";
+import ifBusinessExists from "../helpers/models/businessExists";
+import validateDocumentExistence from "./helpers/valDocExistence";
 
 import CustomError from "../utils/customError";
-import handleCatchError from "../utils/catchErrorHandler";
+import augmentAndForwardError from "../utils/errorAugmenter";
+
+type BusinessCheckOptions =
+	| {
+			checkIn: "body";
+			bodyEntity: string;
+			entity: string;
+			passIfExists: boolean;
+			key: keyof T_idBusiness;
+	  }
+	| {
+			checkIn: "query" | "params";
+			bodyEntity: undefined | null;
+			entity: string;
+			passIfExists: boolean;
+			key: keyof T_idBusiness;
+	  };
 
 // Middleware function to check if a business exists
-const checkForBusiness: RequestHandler = async (req, res, next) => {
-	// Define the function name for error handling
-	const funcName = "checkForBusiness";
+const checkForBusiness = ({
+	checkIn,
+	bodyEntity,
+	entity,
+	passIfExists,
+	key = "name",
+}: BusinessCheckOptions) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		// Define the function name for error handling
+		const collectionName = "Business";
+		const funcName = "checkForBusiness";
 
-	const { businessId } = req.params;
+		let valToKey: string;
+		let businessExists: { _id: mongoose.Types.ObjectId } | null | undefined;
 
-	try {
-		// Try to find the business with the given ID
-		const ifBusiness = await business.findById(businessId);
+		let errMessage: string;
+		try {
+			switch (checkIn) {
+				case "body":
+					valToKey = req.body[bodyEntity][entity];
+					businessExists = await ifBusinessExists(next, {
+						[key]: valToKey,
+					});
 
-		// If the business exists, proceed to the next middleware function
-		if (ifBusiness) {
-			next();
-		} else {
-			// If the business does not exist, create an error message
-			const errMessage = `business-: ${businessId} does not exist.`;
+					validateDocumentExistence({
+						nextFuncion: next,
+						docExists: businessExists,
+						passIfExists: passIfExists,
+						collection: collectionName,
+						collectionAttr: valToKey,
+					});
 
-			// Create a new error with the error message
-			const err = new CustomError(errMessage);
+					break;
 
-			// Set the HTTP status code for the error
-			err.status = 404;
+				case "query":
+					// added for future use
+					break;
 
-			throw err;
+				case "params":
+					valToKey = req.params[entity];
+					businessExists = await ifBusinessExists(next, {
+						[key]: valToKey,
+					});
+
+					validateDocumentExistence({
+						nextFuncion: next,
+						docExists: businessExists,
+						passIfExists: passIfExists,
+						collection: collectionName,
+						collectionAttr: valToKey,
+					});
+
+					break;
+
+				default: {
+					errMessage = `the request could not be completed because the checkIn-: ${checkIn} is not supported.`;
+					const err = new CustomError(errMessage);
+
+					throw err;
+				}
+			}
+		} catch (err) {
+			// Handle the caught error by passing it to the augmentAndForwardError function which will pass it to the error handling middleware
+			augmentAndForwardError({
+				next: next,
+				err: err,
+				funcName: funcName,
+				errStatus: 400,
+			});
 		}
-	} catch (err) {
-		// Handle the caught error by passing it to the handleCatchError function which will pass it to the error handling middleware
-		handleCatchError({ next: next, err: err, funcName: funcName });
-	}
+	};
 };
 
 // Export the middleware function
