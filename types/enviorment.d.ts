d37ff6a31e4980b52e51116c92ec1ce31b15fb1c
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-03-26 14:09:01 +0530

    add types for JWT secret env's

--- types/enviorment.d.ts
+++ types/enviorment.d.ts
@@ -4,6 +4,8 @@ declare global {
             NODE_ENV: "development" | "production";
             MONGO_URI: string;
             API_KEY?: string;
+            JWT_ACCESS_SECRET_KEY: string;
+            JWT_REFRESH_SECRET_KEY: string;
         }
     }
 }
