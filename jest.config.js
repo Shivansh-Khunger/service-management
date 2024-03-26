998c97340be3f37d88657a133a7606f0af2fe20b
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-03-26 14:36:10 +0530

    rename jest.config.ts -> .js due to ts parser error

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+const config = {
+    verbose: true,
+    moduleFileExtensions: ['ts', 'js'],
+    transform: {
+        '^.+\\.(ts)$': [
+            'ts-jest',
+            {
+                tsconfig: 'tsconfig.json',
+            },
+        ],
+    },
+    testMatch: ['**/tests/**/*.test.(js|ts)'],
+    modulePathIgnorePatterns: ['./tests/config', './dist'],
+    moduleNameMapper: {
+        '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+        '^@helpers/(.*)$': '<rootDir>/src/helpers/$1',
+        '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+        '^@models/(.*)$': '<rootDir>/src/models/$1',
+        '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^@validations/(.*)$': '<rootDir>/src/validations/$1',
+        '^@services/(.*)$': '<rootDir>/src/services/$1',
+        '^@logger$': '<rootDir>/src/logger',
+    },
+};
+
+module.exports = config;
