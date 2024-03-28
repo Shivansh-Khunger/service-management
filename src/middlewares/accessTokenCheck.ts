e539de850c19dc4fd7fa044728ca56c9b055d3aa
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-03-28 13:37:26 +0530

    update helper & middleware for new JWT payload

--- src/middlewares/accessTokenCheck.ts
+++ src/middlewares/accessTokenCheck.ts
@@ -1,20 +1,20 @@
 // Import types
-import type { JWT } from "@helpers/createTokens";
-import type { RequestHandler } from "express";
+import type { CustomJwtPayload, JWT } from '@helpers/createTokens';
+import type { RequestHandler } from 'express';
 
 // Import necessary modules
-import { insertJWT } from "@helpers/createCookie";
-import createToken from "@helpers/createTokens";
-import isRefreshTokenValid from "@helpers/validRefreshToken";
-import { logger } from "@logger";
-import CustomError from "@utils/customError";
-import augmentAndForwardError from "@utils/errorAugmenter";
-import jwt from "jsonwebtoken";
+import { insertJWT } from '@helpers/createCookie';
+import createToken from '@helpers/createTokens';
+import isRefreshTokenValid from '@helpers/validRefreshToken';
+import { logger } from '@logger';
+import CustomError from '@utils/customError';
+import augmentAndForwardError from '@utils/errorAugmenter';
+import jwt from 'jsonwebtoken';
 
 // Define the middleware function
 const checkForAccessToken: RequestHandler = (req, res, next) => {
     // Define the function name for logging purposes
-    const funcName = "checkForAccessToken";
+    const funcName = 'checkForAccessToken';
 
     // Extract the access and refresh tokens from the request
     const { accessToken } = req.signedCookies;
@@ -23,7 +23,7 @@ const checkForAccessToken: RequestHandler = (req, res, next) => {
     // Initialize userCredentials object if it doesn't exist
     if (!req.userCredentials) {
         req.userCredentials = {
-            userId: "",
+            userId: '',
         };
     }
 
@@ -31,17 +31,21 @@ const checkForAccessToken: RequestHandler = (req, res, next) => {
     try {
         if (accessToken) {
             // Verify the access token
-            const decoded = jwt.verify(
+            let decoded = jwt.verify(
                 accessToken,
                 process.env.JWT_ACCESS_SECRET_KEY,
             );
 
+            decoded = decoded as CustomJwtPayload;
+
             // Assign the user ID from the decoded token to the request object
-            req.userCredentials.userId = decoded?.sub as string;
+            req.userCredentials.userId = decoded.sub;
+            req.userCredentials.userName = decoded.userData.name;
+            req.userCredentials.userEmail = decoded.userData.email;
 
             // Log the successful verification
             logger.info(
-                "-> the accessToken sent has been successfully verified.",
+                '-> the accessToken sent has been successfully verified.',
             );
 
             // Proceed to the next middleware
@@ -50,14 +54,19 @@ const checkForAccessToken: RequestHandler = (req, res, next) => {
             if (refreshToken) {
                 try {
                     // Validate the refresh token
-                    const refreshDecoded = isRefreshTokenValid(refreshToken);
+                    let refreshDecoded = isRefreshTokenValid(refreshToken);
+                    refreshDecoded = refreshDecoded as CustomJwtPayload;
 
                     // Assign the user ID from the decoded refresh token to the request object
                     req.userCredentials.userId = refreshDecoded?.sub as string;
 
                     // Create the payload for the access token
                     const userAccessTokenPayload: JWT = {
-                        sub: req.userCredentials.userId.toString(), // The subject of the token is the user's ID
+                        sub: refreshDecoded.sub as string, // The subject of the token is the user's ID
+                        userData: {
+                            name: refreshDecoded.userData.name,
+                            email: refreshDecoded.userData.email,
+                        },
                     };
 
                     // Create the access token
@@ -72,7 +81,7 @@ const checkForAccessToken: RequestHandler = (req, res, next) => {
                     // Insert the access token into a cookie
                     insertJWT({
                         res: res, // The response object
-                        field: "accessToken", // The name of the cookie
+                        field: 'accessToken', // The name of the cookie
                         fieldValue: userAccessToken, // The value of the cookie
                         maxAge: userAccessCookieMaxAge, // The max age of the cookie
                     });
@@ -90,7 +99,7 @@ const checkForAccessToken: RequestHandler = (req, res, next) => {
             } else {
                 // If there's no access token, throw an error
                 const err = new CustomError(
-                    "Request could not be authenticated.",
+                    'Request could not be authenticated.',
                 );
                 err.status = 401;
 
@@ -102,21 +111,21 @@ const checkForAccessToken: RequestHandler = (req, res, next) => {
         const error = new CustomError();
 
         // If the error is due to an expired token
-        if ((err as jwt.VerifyErrors).name === "TokenExpiredError") {
+        if ((err as jwt.VerifyErrors).name === 'TokenExpiredError') {
             // Set the error message, status, and log message
             error.message =
-                "Request could not be authenticated due to expired access token.";
+                'Request could not be authenticated due to expired access token.';
             error.status = 401;
-            error.logMessage = "an expired access token has been received.";
+            error.logMessage = 'an expired access token has been received.';
         }
 
         // If the error is due to an invalid token
-        if ((err as jwt.VerifyErrors).name === "JsonWebTokenError") {
+        if ((err as jwt.VerifyErrors).name === 'JsonWebTokenError') {
             // Set the error message, status, and log message
             error.message =
-                "Request could not be authenticated due to invalid access token.";
+                'Request could not be authenticated due to invalid access token.';
             error.status = 401;
-            error.logMessage = "an invalid access token has been received.";
+            error.logMessage = 'an invalid access token has been received.';
         }
 
         // Augment and forward the error
